1662c15b910d77512f5ab0f7723aecd887aec7a7
Timothy Saudners <timsaunders88@gmail.com>
2020-08-27 15:40:32 +0100

    MVP

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,41 @@
 <template>
     <div>
         <h1>UK Energy Mix</h1>
-        <display-chart></display-chart>
+        <display-chart :generationMix="generationMix"></display-chart>
     </div>
 </template>
 
 <script>
-export default {
+import DisplayChart from './components/DisplayChart.vue'
 
+export default {
+  name: 'app',
+  data(){
+    return {
+      generationMix: []
+      }
+  },
+  mounted(){
+    this.fetchData()
+  },
+  methods:{
+      fetchData(){
+          fetch('https://api.carbonintensity.org.uk/generation')
+          .then(resp => resp.json())
+          .then(data => this.generationMix = this.formatForChart(data.data.generationmix))
+      },
+     
+      formatForChart(objectList) {
+        const newList = objectList.map((object)=>{
+          return [object.fuel, object.perc]})
+        newList.unshift(["Fuel", "Percentage"])
+        return newList
+      }
+  },
+  components: {
+    'display-chart': DisplayChart
+  }
+      
 }
 </script>
 
---
+++ src/components/DisplayChart.vue
@@ -0,0 +1,22 @@
+<template>
+    <GChart
+    type="PieChart"
+    :data="generationMix"
+    :options="chartOptions"
+  />
+</template>
+
+<script>
+import { GChart } from 'vue-google-charts'
+
+export default {
+  components: {
+    GChart
+  },
+  props:['generationMix']
+}
+</script>
+
+<style>
+
+</style>
